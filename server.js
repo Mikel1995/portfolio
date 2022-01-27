92bbd9fcdd4454cfc60a7ec589670992dce26b51
Mikel Kasneci <mkasneci0@gmail.com>
2022-01-27 17:20:15 +0100

    add serveStatic

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(path.join(__dirname, 'dist'));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 
 app.listen(port);
